ff09dcedb51270452c2043d0a7cc2461e633223e
Terts Diepraam <terts.diepraam@gmail.com>
2024-03-01 15:40:27 +0100

    updates

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,11 @@
 BRANCH=$(git branch --show-current HEAD)
 echo "blaaaa: $BRANCH"
-if [ "$BRANCH" = "main" ]; then
-    read -p "Are you sure you want to commit to main??? [y/n]: " yn
-    case $yn in
-        [Yy]*) echo "Ok, fine, you do you"; exit 0 ;;  
-        [Nn]*) echo "Good!"; exit 1 ;;
-    esac
-fi
+
+case "$BRANCH" in
+    *main*)
+        read -p "Are you sure you want to commit to main??? [y/n]: " yn
+        case $yn in
+            [Yy]*) echo "Ok, fine, you do you"; exit 0 ;;  
+            [Nn]*) echo "Good!"; exit 1 ;;
+        esac;;
+esac
